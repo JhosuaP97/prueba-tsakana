9e26159dd8b5368c3823c39ecd2e0efabd40656a
Jhosua Pachon <jhosuapachon@gmail.com>
2021-11-05 00:24:03 -0500

    add array with options to Navbar

---
+++ src/utils/navbarOptions.js
@@ -0,0 +1,38 @@
+export const listDropDown = [
+  {
+    title: "Nuevos",
+    arrowDown: true,
+  },
+  {
+    title: "Jeans",
+    arrowDown: true,
+  },
+  {
+    title: "Ropa",
+    arrowDown: true,
+  },
+  {
+    title: "Zapatos",
+    arrowDown: true,
+  },
+  {
+    title: "Vestidos",
+    arrowDown: true,
+  },
+  {
+    title: "Accesorios",
+    arrowDown: true,
+  },
+  {
+    title: "Básicos",
+    arrowDown: false,
+  },
+  {
+    title: "Colecciones",
+    arrowDown: false,
+  },
+  {
+    title: "Descuentos",
+    arrowDown: false,
+  },
+];
