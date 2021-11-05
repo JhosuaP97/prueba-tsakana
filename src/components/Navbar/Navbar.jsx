290d130620fab07a16376e40870c1badb4ac7c38
Jhosua Pachon <jhosuapachon@gmail.com>
2021-11-05 00:21:28 -0500

    add Navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React, { forwardRef } from "react";
+import { ArrowDown } from "../../assets";
+import { listDropDown } from "../../utils/navbarOptions";
+import "./style.css";
+
+const Navbar = ({ open }, ref) => {
+  console.log({ open });
+  return (
+    <nav className="nav">
+      <ul className={`${open ? "open" : "close"}`} ref={ref}>
+        {listDropDown.map((item) => (
+          <li>
+            {item.title}
+            {item.arrowDown === true && (
+              <ArrowDown width="10px" height="10px" />
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+const forwardRefNavbar = forwardRef(Navbar);
+
+export default forwardRefNavbar;
