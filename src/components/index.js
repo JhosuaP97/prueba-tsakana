e5224965721fba1c708ab86527a7b6703a55ba34
Jhosua Pachon <jhosuapachon@gmail.com>
2021-11-05 00:23:26 -0500

    add import components

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header/Header";
+export { default as Search } from "./Search/Search";
+export { default as ShoppingBag } from "./ShoppingBag/ShoppingBag";
+export { default as Navbar } from "./Navbar/Navbar";
